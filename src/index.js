0d28b37fbd3c77b57f45266f2216bb98c630ba94
hovanhoa <hovanhoaa1@gmail.com>
2023-11-17 13:54:39 +0700

    feat: add greeting app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals(sendToVercelAnalytics);
